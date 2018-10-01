65f4a81cd68244bcc6daa43e1372309ff347769a
Iggy Drougge <idrougge@mac.com>
2018-10-01 17:15:18 +0200

    Initial Commit

---
+++ OutlineView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OutlineView
+//
+//  Created by Iggy Drougge on 2018-10-01.
+//  Copyright © 2018 Iggy Drougge. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ OutlineView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OutlineView
+//
+//  Created by Iggy Drougge on 2018-10-01.
+//  Copyright © 2018 Iggy Drougge. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
