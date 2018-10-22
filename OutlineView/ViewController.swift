4f74bb66dc92ed4b05cbc3150ce7917920e12a29
Iggy Drougge <idrougge@mac.com>
2018-10-22 11:53:45 +0200

    Bind value column as well, only populate children if children > 1

--- OutlineView/ViewController.swift
+++ OutlineView/ViewController.swift
@@ -12,15 +12,9 @@ class ViewController: NSViewController {
 
     @IBOutlet var treeController: NSTreeController!
     
-    @objc dynamic let dict: [String:Any] = ["apa": 1, "bpa": 2, "cpa": ["ett": 1, "två": 2], "dpa": "fyra"]
-    @objc dynamic var arr: [String] {
-        return dict.keys.map{String($0)}
-    }
-    @objc dynamic var kaka = ["apa": 1, "bpa": 2, "cpa": ["ett": 1, "två": 2], "dpa": "fyra"].map(TreeItem.init)
+    //@objc dynamic let dict: [String:Any] = ["apa": 1, "bpa": 2, "cpa": ["ett": 1, "två": 2], "dpa": "fyra"]
     
-    @objc dynamic var children: [String] {
-        return ["hej"]
-    }
+    @objc dynamic var kaka = ["apa": 1, "bpa": 2, "cpa": ["ett": 1, "två": 2], "dpa": "fyra"].map(TreeItem.init)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,22 +32,27 @@ class ViewController: NSViewController {
 @objc class TreeItem: NSObject {
     @objc var name: String//? = nil
     @objc var children: [TreeItem] = []
+    @objc var value: String?
     @objc var count: Int { return children.count }
     @objc var isLeaf: Bool { return children.isEmpty }
     init(_ obj: Any) {
         //print("obj:",obj, obj is (String,Int))
         switch obj {
         case let (k,v) as (String,Int):
-            print("k:\(k),nr:\(v)"); self.name = k; self.children = [TreeItem(v)]
+            print("k:\(k),nr:\(v)")
+            self.name = k
+            self.value = "\(v)"
         case let (k,v) as (String,String):
-            print("k:\(k),str:\(v)"); self.name = k; self.children = [TreeItem(v)]
+            print("k:\(k),str:\(v)")
+            self.name = k
+            self.value = v
         case let (k,v) as (String,[String:Any]):
-            print("k:\(k),dic:\(v)"); self.name = k; self.children = v.map(TreeItem.init)
-            //        case let (k,v) as (String,Any): print("k:\(k),v:\(v)"); self.name = k; self.children = [TreeItem(v)]
-            //        case let nr as Int: print("nr: \(nr)"); self.name = "\(nr)"
-            //        case let str as String: print("str: \(str)"); self.name = str
-        //        case let dict as [String:Any]: print("dict:\(dict)"); children = dict.map(TreeItem.init)
-        default: self.name = "\(obj)"
+            print("k:\(k),dic:\(v)")
+            self.name = k
+            let children = v.map(TreeItem.init)
+            self.value = "\(children.count) items"
+            self.children = children
+        default: self.name = "Error: \(obj)"
         }
     }
 }
