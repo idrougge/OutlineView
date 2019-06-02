27d065d5dc2ebe6499d27b74c4cb895260ba1d80
Iggy Drougge <idrougge@mac.com>
2019-06-02 21:56:52 +0200

    Indent static JSON

--- OutlineView/ViewController.swift
+++ OutlineView/ViewController.swift
@@ -10,26 +10,26 @@ import Cocoa
 
 let json = """
 {
-"glossary": {
-"title": "example glossary",
-"GlossDiv": {
-"title": "S",
-"GlossList": {
-"GlossEntry": {
-"ID": "SGML",
-"SortAs": "SGML",
-"GlossTerm": "Standard Generalized Markup Language",
-"Acronym": "SGML",
-"Abbrev": "ISO 8879:1986",
-"GlossDef": {
-"para": "A meta-markup language, used to create markup languages such as DocBook.",
-"GlossSeeAlso": ["GML", "XML"]
-},
-"GlossSee": "markup"
-}
-}
-}
-}
+    "glossary": {
+        "title": "example glossary",
+        "GlossDiv": {
+            "title": "S",
+            "GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+                    "SortAs": "SGML",
+                    "GlossTerm": "Standard Generalized Markup Language",
+                    "Acronym": "SGML",
+                    "Abbrev": "ISO 8879:1986",
+                    "GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+                        "GlossSeeAlso": ["GML", "XML"]
+                    },
+                    "GlossSee": "markup"
+                }
+            }
+        }
+    }
 }
 """
 class ViewController: NSViewController {
